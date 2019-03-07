6ecc542c2d76956060f280ac32c1e9f46fbb9dfe
etcart <amberhosen@gmail.com>
2019-03-07 16:32:30 +0200

    '_' symbol as an invalid start extended to entityAttribute

--- ObjectToJson/entityAttribute.py
+++ ObjectToJson/entityAttribute.py
@@ -101,6 +101,9 @@ class EntityAttribute():
             self.setPythonMetaData(ipmd, "class")
             tempDict = {}
             for key in iterL:
+                if key.startswith('_'):
+                    continue
+                
                 tempDict[key] = EntityAttribute(getattr(_object, key), ipmd)
             self.value = tempDict
 
