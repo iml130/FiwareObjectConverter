b3af95f9b31090c1b20184ff654cac58959a3edd
Aaron Rüthing <aaron.ruthing@googlemail.com>
2021-02-10 15:48:02 +0100

    Adapt normalize functions
    Adapt normalize functions, so that the function is also called for each element
    in a data-structure like a list or an dictionary.
    Also inner an NGSI-LD representation objects now take a type in their value,
    so it can be converted back correctly.

--- fiwareobjectconverter/json_to_object/normalize_to_v2.py
+++ fiwareobjectconverter/json_to_object/normalize_to_v2.py
@@ -3,7 +3,8 @@
 class V2_Normalizer:
     @classmethod
     def _ngsiv2_uri(cls, ld_uri):
-        return ld_uri.split('urn:ngsi-ld:')[1]
+        ld_uri = ld_uri.split('urn:ngsi-ld:')[1]
+        return ld_uri.split(':')[1]
 
     @classmethod
     # Generates a Relationship's object as a URI
@@ -35,46 +36,95 @@ class V2_Normalizer:
                 continue
 
             attr = entity[key]
-            out[key] = {}
-            v2_attr = out[key]
-
-            if not('type' in attr) or attr['type'] != 'Relationship':
-                v2_attr['type'] = 'Property'
-                v2_attr['value'] = attr['value']
-            else:
-                ref_key = 'ref'+str(key)
-                v2_attr = out[ref_key]
-                v2_attr['type'] = 'Reference'
-                aux_obj = attr['object']
-                if isinstance(aux_obj, list):
-                    v2_attr['value'] = list()
-                    for obj in aux_obj:
-                        v2_attr['value'].append(cls._v2_object(obj))
-                else:
-                    v2_attr['value'] = cls._v2_object(str(aux_obj))
-
-            #if key == 'location':
-            #    ld_attr['type'] = 'GeoProperty'
-            #
-            #if 'type' in attr and attr['type'] == 'DateTime':
-            #    ld_attr['value'] = {
-            #        '@type': 'DateTime',
-            #        '@value': normalize_date(attr['value'])
-            #    }
-            #
-            #if 'type' in attr and attr['type'] == 'PostalAddress':
-            #    ld_attr['value']['type'] = 'PostalAddress'
-
-            if len(attr)>2:
-                v2_attr['metadata'] = {}
-                for mkey in attr:
-                    if mkey not in ('type', 'value'):
-                        sub_attr = dict()
-                        sub_attr['type'] = 'Property'
-                        sub_attr['value'] = attr[mkey]['value']
-                        v2_attr['metadata'][mkey] = sub_attr
+            out[key] = cls._normalize_attribute(key, attr)
         return out
 
+    @classmethod
+    def _normalize_attribute(cls, key, attr):
+        v2_attr = {}
+        if not('type' in attr) or attr['type'] != 'Relationship':
+            if key == 'python':
+                v2_attr['type'] = 'dataType'
+            else:
+                obj_ = attr['value']
+                if isinstance(obj_, str):
+                    type_ = 'string'
+                elif isinstance(obj_, bool):
+                    type_ = 'boolean'
+                elif isinstance(obj_, int):
+                    type_ = 'number'
+                elif isinstance(obj_, float):
+                    type_ = 'number'
+                elif isinstance(obj_, complex):
+                    type_ = 'array'
+                elif isinstance(obj_, tuple):
+                    type_ = 'array'
+                elif isinstance(obj_, list):
+                    type_ = 'array'
+                elif isinstance(obj_, dict):
+                    if 'type' in obj_.keys():
+                        type_ = obj_['type']
+                    else:
+                        type_ = 'object'
+                else:
+                    if obj_['type']:
+                        type_ = obj_['type']
+                    else:
+                        type_ = 'object'
+                v2_attr['type'] = type_
+
+            if v2_attr['type'] in ['string', 'number', 'boolean']:
+                v2_attr['value'] = attr['value']
+            elif v2_attr['type'] == 'array':
+                v2_attr['value'] = []
+                obj_attr = attr['value']
+                for new_key in obj_attr:
+                    v2_attr['value'].append(
+                        cls._normalize_attribute(None, new_key))
+            else:
+                v2_attr['value'] = {}
+                com_attr = attr['value']
+                for new_key in com_attr:
+                    if new_key != 'type':
+                        v2_attr['value'][new_key] = cls._normalize_attribute(
+                            new_key, com_attr[new_key])
+        else:
+            #ref_key = 'ref'+str(key)
+            #v2_attr = out[ref_key]
+            v2_attr['type'] = 'Reference'
+            aux_obj = attr['object']
+            if isinstance(aux_obj, list):
+                v2_attr['value'] = list()
+                for obj in aux_obj:
+                    v2_attr['value'].append(cls._v2_object(obj))
+            else:
+                v2_attr['value'] = cls._v2_object(str(aux_obj))
+
+        # if key == 'location':
+        #    ld_attr['type'] = 'GeoProperty'
+        #
+        # if 'type' in attr and attr['type'] == 'DateTime':
+        #    ld_attr['value'] = {
+        #        '@type': 'DateTime',
+        #        '@value': normalize_date(attr['value'])
+        #    }
+        #
+        # if 'type' in attr and attr['type'] == 'PostalAddress':
+        #    ld_attr['value']['type'] = 'PostalAddress'
+
+        if len(attr) > 2:
+            metadata = {}
+            for mkey in attr:
+                if mkey == 'v2_type':
+                    continue
+                if mkey not in ('type', 'value'):
+                    sub_attr = dict()
+                    sub_attr['type'] = 'dataType'
+                    sub_attr['value'] = attr[mkey]['value']
+                    metadata[mkey] = sub_attr
+            if len(metadata) > 0:
+                v2_attr['metadata'] = metadata
+        return v2_attr
 
     @classmethod
     def _normalize_date(cls, date_str):
--- fiwareobjectconverter/object_to_json/normalize_to_ld.py
+++ fiwareobjectconverter/object_to_json/normalize_to_ld.py
@@ -11,12 +11,12 @@ Author: José Manuel Cantera
 from rfc3987 import parse
 etsi_core_context = 'https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld'
 
+
 class LD_Normalizer:
     @classmethod
     def _ngsild_uri(cls, type_part, id_part):
         template = 'urn:ngsi-ld:{}:{}'
         return template.format(type_part, id_part)
-
 
     # Generates an Entity Id as a URI
     @classmethod
@@ -31,7 +31,6 @@ class LD_Normalizer:
             out = cls._ngsild_uri(entity_type, entity_id)
 
         return out
-
 
     # Generates a Relationship's object as a URI
     @classmethod
@@ -50,7 +49,6 @@ class LD_Normalizer:
             out = cls._ngsild_uri(entity_type, entity_id)
 
         return out
-
 
     # Do all the transformation work
     @classmethod
@@ -82,50 +80,67 @@ class LD_Normalizer:
                 continue
 
             attr = entity[key]
-            out[key] = {}
-            ld_attr = out[key]
-
-            if not('type' in attr) or attr['type'] != 'Relationship':
-                ld_attr['type'] = 'Property'
-                ld_attr['value'] = attr['value']
-            else:
-                ld_attr['type'] = 'Relationship'
-                aux_obj = attr['value']
-                if isinstance(aux_obj, list):
-                    ld_attr['object'] = list()
-                    for obj in aux_obj:
-                        ld_attr['object'].append(cls._ld_object(key, obj))
-                else:
-                    ld_attr['object'] = cls._ld_object(key, str(aux_obj))
-
-            #if key == 'location':
-            #    ld_attr['type'] = 'GeoProperty'
-            #
-            #if 'type' in attr and attr['type'] == 'DateTime':
-            #    ld_attr['value'] = {
-            #        '@type': 'DateTime',
-            #        '@value': cls._normalize_date(attr['value'])
-            #    }
-            #
-            #if 'type' in attr and attr['type'] == 'PostalAddress':
-            #    ld_attr['value']['type'] = 'PostalAddress'
-
-            if 'metadata' in attr:
-                metadata = attr['metadata']
-
-                for mkey in metadata:
-                    if mkey == 'timestamp':
-                        ld_attr['observedAt'] = cls._normalize_date(metadata[mkey]['value'])
-                    elif mkey == 'unitCode':
-                        ld_attr['unitCode'] = metadata[mkey]['value']
-                    else:
-                        sub_attr = dict()
-                        # Metadata which are Relationships is assumed not to be there
-                        sub_attr['type'] = 'Property'
-                        sub_attr['value'] = metadata[mkey]['value']
-                        ld_attr[mkey] = sub_attr
+            out[key] = cls._normalize_attribute(key, attr)
         return out
 
+    @classmethod
+    def _normalize_attribute(cls, key, attr):
+        ld_attr = {}
+        if not('type' in attr) or attr['type'] != 'Relationship':
+            ld_attr['type'] = 'Property'
+            if attr['type'] in ['string', 'number', 'boolean']:
+                ld_attr['value'] = attr['value']
+            elif attr['type'] == 'array':
+                ld_attr['value'] = []
+                obj_attr = attr['value']
+                for new_key in obj_attr:
+                    ld_attr['value'].append(
+                        cls._normalize_attribute(None, new_key))
+            else:
+                ld_attr['value'] = {}
+                obj_attr = attr['value']
+                for new_key in obj_attr:
+                    ld_attr['value'][new_key] = cls._normalize_attribute(
+                        new_key, obj_attr[new_key])
+                ld_attr['value']['type'] = attr['type']
+        else:
+            ld_attr['type'] = 'Relationship'
+            aux_obj = attr['value']
+            if isinstance(aux_obj, list):
+                ld_attr['object'] = list()
+                for obj in aux_obj:
+                    ld_attr['object'].append(cls._ld_object(key, obj))
+            else:
+                ld_attr['object'] = cls._ld_object(key, str(aux_obj))
+
+        # if key == 'location':
+        #    ld_attr['type'] = 'GeoProperty'
+        #
+        # if 'type' in attr and attr['type'] == 'DateTime':
+        #    ld_attr['value'] = {
+        #        '@type': 'DateTime',
+        #        '@value': cls._normalize_date(attr['value'])
+        #    }
+        #
+        # if 'type' in attr and attr['type'] == 'PostalAddress':
+        #    ld_attr['value']['type'] = 'PostalAddress'
+
+        if 'metadata' in attr:
+            metadata = attr['metadata']
+
+            for mkey in metadata:
+                if mkey == 'timestamp':
+                    ld_attr['observedAt'] = cls._normalize_date(
+                        metadata[mkey]['value'])
+                elif mkey == 'unitCode':
+                    ld_attr['unitCode'] = metadata[mkey]['value']
+                else:
+                    sub_attr = dict()
+                    # Metadata which are Relationships is assumed not to be there
+                    sub_attr['type'] = 'Property'
+                    sub_attr['value'] = metadata[mkey]['value']
+                    ld_attr[mkey] = sub_attr
+        return ld_attr
 
     @classmethod
     def _normalize_date(cls, date_str):
